6f6ad5144888b100670099b407c665fb3ffd80f9
Soundryacedcoss <soundryarani@cedcoss.com>
2022-09-19 17:33:56 +0530

    Done

---
+++ script.js
@@ -0,0 +1,20 @@
+var a=document.getElementById("input").value
+
+// function fun(){
+//     var str
+//     // document.getElementById("p1").innerHTML=;
+//     Array.from(a);
+//     a.forEach(element => {
+//         console.log(Array[0])
+//     });
+    
+// }
+const str="india";
+// str.split("");
+Object.assign([],str);
+console.log(str)
+
+// document.write(str);
+for(i=0;i<=length;i++){
+    console.log(str[0])
+}
